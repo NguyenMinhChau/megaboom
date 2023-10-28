c8f0a9e616ccf0940278b5db1aed6cb26f808e14
NguyenMinhChau <nmchauhcmue@gmail.com>
2023-10-28 12:13:25 +0700

    push code

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -2,7 +2,7 @@
 import { styled } from '@mui/material';
 import Branding from '@/assets/images/product_cta_outLibrary/branding.png';
 import ECOSYSTEM from '@/assets/images/product_cta_outLibrary/ecocta.png';
-import Marketing from '@/assets/images/product_cta_outLibrary/Marketing.png';
+import Marketing from '@/assets/images/product_cta_outLibrary/marketingcta.png';
 import React from 'react';
 import ProductItem from './ProductItem';
 import { PRIMARY_COLOR } from '@/styles/color.global';
