03ccd17e5312b7e676e2fb3efcd7b689705dd238
NguyenMinhChau <nmchauhcmue@gmail.com>
2023-10-28 11:12:35 +0700

    push code

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<head>
-				<meta charSet="utf-8" />
+				{/* <meta charSet="utf-8" /> */}
 				<meta
 					content="width=device-width, initial-scale=1"
 					name="viewport"
